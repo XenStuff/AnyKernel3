39fdcde4fa5109649df08ce4d0f7c9b67e913ae3
xenxynon <flynryder427@gmail.com>
2022-04-14 06:57:34 +0000

    neko

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Predator Stormbreaker Kernel  by sohamsen  @ xda-developers
+kernel.string=NekoKernel-Lite by @Ratoriku
 do.devicecheck=1
 do.modules=0
 do.systemless=1
